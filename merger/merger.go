f0df26058082fac694116e116f0250a9a3ac6b71
evg1605 <evg1605@gmail.com>
2021-03-26 10:04:46 +0800

    merge async

--- merger/merger.go
+++ merger/merger.go
@@ -1,8 +1,19 @@
 package merger
 
+import (
+	"github.com/evg1605/async-lab/workers_pool"
+	"runtime"
+	"sync"
+)
+
 type Source struct {
-	GroupName     string
-	ObjProperties map[string]string
+	GroupName        string
+	ObjectsWithValue map[string]string
+}
+
+type resultWithCount struct {
+	countGroups int
+	result      Result
 }
 
 type Result = map[string]map[string]string
@@ -12,12 +23,83 @@ func MergeSync(sources []*Source) (Result, error) {
 
 	for _, src := range sources {
 
-		for name, val := range src.ObjProperties {
+		for name, val := range src.ObjectsWithValue {
 			if _, ok := res[name]; !ok {
 				res[name] = make(map[string]string)
 			}
 			res[name][src.GroupName] = val
 		}
 	}
+	return res, nil
+}
+
+func MergeAsyncWithWp(sources []*Source) (Result, error) {
+	res := make(Result)
+
+	wp := workers_pool.StartNewPool(runtime.NumCPU())
+	for _, src := range sources {
+
+		for name, _ := range src.ObjectsWithValue {
+			if _, ok := res[name]; ok {
+				continue
+			}
+
+			objName := name
+			m := make(map[string]string)
+			res[objName] = m
+			_ = wp.AddJob(func() {
+				for _, src := range sources {
+					if objVal, ok := src.ObjectsWithValue[objName]; ok {
+						m[src.GroupName] = objVal
+					}
+				}
+			})
+		}
+	}
+	wp.WaitJobsAndStop()
+	return res, nil
+}
+
+func MergeAsync(sources []*Source) (Result, error) {
+	res := make(Result)
+
+	countWorkers := runtime.NumCPU()
+	objCh := make(chan struct {
+		name string
+		m    map[string]string
+	}, countWorkers*2)
+	wg := &sync.WaitGroup{}
+	wg.Add(countWorkers)
+
+	for i := 0; i < countWorkers; i++ {
+		go func() {
+			defer wg.Done()
+			for obj := range objCh {
+				for _, src := range sources {
+					if objVal, ok := src.ObjectsWithValue[obj.name]; ok {
+						obj.m[src.GroupName] = objVal
+					}
+				}
+			}
+		}()
+	}
+
+	for _, src := range sources {
+		for name, _ := range src.ObjectsWithValue {
+			if _, ok := res[name]; ok {
+				continue
+			}
+
+			m := make(map[string]string)
+			res[name] = m
+			objCh <- struct {
+				name string
+				m    map[string]string
+			}{name: name, m: m}
+		}
+	}
+	close(objCh)
+
+	wg.Wait()
 	return res, nil
 }
--- merger/merger_test.go
+++ merger/merger_test.go
@@ -1,33 +1,37 @@
 package merger
 
 import (
-	"github.com/stretchr/testify/require"
+	"fmt"
+	"log"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestMergeSync(t *testing.T) {
-
-	sources := []*Source{
+func createSources1() []*Source {
+	return []*Source{
 		{
 			GroupName: "gr1",
-			ObjProperties: map[string]string{
+			ObjectsWithValue: map[string]string{
 				"item1": "gr1val for item1",
 				"item2": "gr1val for item2",
 				"item3": "gr1val for item3"},
 		}, {
 			GroupName: "gr2",
-			ObjProperties: map[string]string{
+			ObjectsWithValue: map[string]string{
 				"item1": "gr2val for item1",
 				"item2": "gr2val for item2",
 				"item3": "gr2val for item3"},
 		}, {
 			GroupName: "gr3",
-			ObjProperties: map[string]string{
+			ObjectsWithValue: map[string]string{
 				"item3": "gr3val for item3"},
 		}}
+}
 
-	res, err := MergeSync(sources)
-	require.NoError(t, err)
+func checkResult1(t *testing.T, res Result) {
+
 	require.NotNil(t, res)
 
 	require.Len(t, res, 3)
@@ -44,4 +48,90 @@ func TestMergeSync(t *testing.T) {
 	require.Contains(t, res["item3"], "gr3")
 
 	require.Equal(t, res["item3"]["gr2"], "gr2val for item3")
+}
+
+func createBigSources(groupsCount, itemsCount int) []*Source {
+	sources := make([]*Source, groupsCount)
+	for groupNum := 0; groupNum < groupsCount; groupNum++ {
+		src := &Source{
+			GroupName:        fmt.Sprintf("group-%v", groupNum),
+			ObjectsWithValue: make(map[string]string),
+		}
+		for itemNum := 0; itemNum < itemsCount; itemNum++ {
+			src.ObjectsWithValue[fmt.Sprintf("item-%v", itemNum)] = fmt.Sprintf("item-%v val for group %v", itemNum, groupNum)
+		}
+		sources[groupNum] = src
+	}
+	return sources
+}
+
+func TestMergeSync(t *testing.T) {
+
+	sources := createSources1()
+
+	res, err := MergeSync(sources)
+
+	require.NoError(t, err)
+	checkResult1(t, res)
+}
+
+func TestMergeAsyncWithWp(t *testing.T) {
+
+	sources := createSources1()
+
+	res, err := MergeAsyncWithWp(sources)
+
+	require.NoError(t, err)
+	checkResult1(t, res)
+}
+
+func TestMergeAsync(t *testing.T) {
+
+	sources := createSources1()
+
+	res, err := MergeAsync(sources)
+
+	require.NoError(t, err)
+	checkResult1(t, res)
+}
+
+func TestMergeCompare(t *testing.T) {
+	sources := createBigSources(1000, 1000)
+
+	log.Println("MergeAsyncWithWp")
+	now := time.Now()
+	_, _ = MergeAsyncWithWp(sources)
+	log.Println(time.Now().Sub(now).String())
+
+	log.Println("MergeAsync")
+	now = time.Now()
+	_, _ = MergeAsync(sources)
+	log.Println(time.Now().Sub(now).String())
+
+	log.Println("MergeSync")
+	now = time.Now()
+	_, _ = MergeSync(sources)
+	log.Println(time.Now().Sub(now).String())
+
+}
+
+func BenchmarkMergeAsyncWithWp(b *testing.B) {
+	sources := createBigSources(1000, 500)
+	b.ResetTimer()
+
+	_, _ = MergeAsyncWithWp(sources)
+}
+
+func BenchmarkMergeAsync(b *testing.B) {
+	sources := createBigSources(1000, 500)
+	b.ResetTimer()
+
+	_, _ = MergeAsync(sources)
+}
+
+func BenchmarkMergeSync(b *testing.B) {
+	sources := createBigSources(1000, 500)
+	b.ResetTimer()
+
+	_, _ = MergeSync(sources)
 }
